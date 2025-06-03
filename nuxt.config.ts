d6bba6feb21581d8c3fea0a7e4af24a5828a255c
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2025-06-02 21:19:52 -0400

    Fix head meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/content'],
-  css: ['~/assets/css/main.css'],
-})
+  app: {
+    head: {
+      title: "Zachary Mayhew",
+      htmlAttrs: {
+        lang: "en",
+      },
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+  modules: ["@nuxt/eslint", "@nuxt/content"],
+  css: ["~/assets/css/main.css"],
+});
